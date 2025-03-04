fd0b30b3a97173b5354e734f069ef693add88185
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-04 11:47:50 +0530

    Changed namespace

--- Namespaces/gpu.cpp
+++ Namespaces/gpu.cpp
@@ -25,6 +25,18 @@ namespace gpu{
         char array[number_of_characters];
     };
 
+    typedef struct float512_t
+    {
+        uint512_t integer;
+        uint512_t fraction;
+    };
+
+    typedef struct float1024_t
+    {
+        uint1024_t integer;
+        uint1024_t fraction;
+    };
+
     typedef struct gpu_information
     {
         uint1024_t gpu_start_address_in_RAM;
@@ -37,13 +49,61 @@ namespace gpu{
 
     typedef struct gpu_data
     {
-        uint1024_t shader_image[];
-        uint1024_t        
+        uint1024_t shader_image[7680][4320];
+        uint1024_t red_image[7680][4320];
+        uint1024_t blue_image[7680][4320];
+        uint1024_t green_image[7680][4320];
+        uint1024_t yellow_image[7680][4320];
+        uint1024_t pixel_saturation[7680][4320];
     };
 
     typedef struct object_data
     {
         uint1024_t image [7680][4320];
-        
+        uint1024_t x_coordinate_in_world_model;
+        uint1024_t y_coordinate_in_world_mode;
+        uint1024_t z_coordinate_in_world_model;
+        uint1024_t angle_with_respect_to_plane;
+        uint1024_t electric_field;
+        uint1024_t magnetic_field;
+        uint1024_t radius_of_EM_field;
+        uint1024_t weight_of_object;
+        uint1024_t number_of_vertices;
+        uint1024_t graph_of_object [number_of_vertices];
+    };
+
+    typedef struct gpu_vector
+    {
+        uint1024_t length_of_vector;
+        uint1024_t vector[length_of_vector];
+    };
+
+    typedef struct tensor
+    {
+        uint1024_t length_of_material;
+        uint1024_t first_dimension [length_of_material];
+        uint1024_t second_dimension [length_of_material];
+        uint1024_t third_dimension [length_of_material];
+    };
+
+    typedef time_vector
+    {
+        uint1024_t time_flow;
+        uint1024_t number_of_events;
+        uint1024_t time_flow_array[number_of_events];
+    };
+
+    class vector_calculus{
+        public:
+            
+    };
+
+    class dijkstra
+    {
+
+    };
+
+    class bellman_ford{
+
     };
 }
