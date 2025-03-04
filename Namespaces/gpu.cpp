cc84e5c335f1a2ace0f5de65aab7d771fe90b811
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-04 10:28:40 +0530

    Added a namespace for the gpu

---
+++ Namespaces/gpu.cpp
@@ -0,0 +1,16 @@
+namespace gpu{
+
+    typedef uint512_t = "0x000000000000000000000000000000";
+
+    typedef struct gpu_information
+    {
+        uint64_t gpu_start_address;
+        uint64_t gpu_end_address;
+
+    };
+
+    typedef gpu_data_allocator
+    {
+
+    };
+}
