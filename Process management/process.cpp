4c5c3be579547c5b874561129fed71a0d15a5918
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-06 10:25:57 +0530

    Changed contents of process.cpp file and changed name of scheduler.cpp

---
+++ Process management/process.cpp
@@ -0,0 +1,13 @@
+struct process
+{
+
+    long long int *process_id;// A pointer to the process id stored in RAM.
+    char *process_name;
+    char *process_block;
+    vector<char*> services_offered;
+};
+
+class process
+{
+
+};
