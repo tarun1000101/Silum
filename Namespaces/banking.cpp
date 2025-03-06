06434eaab2c9ce7fee440b38f4a11494d22d432a
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-03-06 16:54:04 +0530

    Added initial functionality for UPI

---
+++ Namespaces/banking.cpp
@@ -0,0 +1,9 @@
+namespace banking{
+        
+    typedef struct bank_information{
+        char *bank_name;
+        char *bank_account_number;
+        char *bank_regional_code;
+        char *bank_account_holder_name;
+    };
+}
---
+++ UPI/banking.cpp
@@ -0,0 +1,14 @@
+//Memory efficiency - how much does it use in RAM (bit or byte wise). --> Very simple once you understand it. You can use memory complexity for that.
+//Compute efficiency - how long does it take to run in the CPU. --> time complexity.
+//GPU efficiency - How quickly can you execute in the gpu. --> time complexity.
+//GPU Memory efficiency - how much VRAM does it take in the GPU. --> Memory complexity.
+
+class banking{
+
+    public:
+        bank_information get_banking_information(char *mobile_number)
+        {
+            bank_information bank_data = new bank_information();
+            return bank_information;
+        }
+};
